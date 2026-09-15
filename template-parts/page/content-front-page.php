b589c49a0d78798b703177d6f9f309c10ad74a1b
Elvin Acuña <elvinwaz@outlook.com>
2026-09-14 23:10:08 -0700

    feat: use full-width page layout

--- template-parts/page/content-front-page.php
+++ template-parts/page/content-front-page.php
@@ -8,7 +8,7 @@
  */
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( 'front-page-entry' ); ?>>
-	<div id="front-page-content" class="container content-narrow">
+	<div id="front-page-content">
 		<div class="entry-content"><?php the_content(); ?></div>
 	</div>
 </article>
